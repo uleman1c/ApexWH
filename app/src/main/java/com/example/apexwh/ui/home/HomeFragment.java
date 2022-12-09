a47d7eee89c2cdca5358a62e96c89f0eed725a0f
Mihail Ulyanenkov <uleman@mail.ru>
2022-12-09 22:13:23 +0300

    home btns ret 2

--- app/src/main/java/com/example/apexwh/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/apexwh/ui/home/HomeFragment.java
@@ -44,8 +44,8 @@ public class HomeFragment extends Fragment {
         });
 
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 

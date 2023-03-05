3a8914424128acc5eb12d89b7f0ec1b31d69bfa2
Mihail Ulyanenkov <uleman@mail.ru>
2023-03-05 13:19:31 +0300

    test products 2

--- app/src/main/java/com/example/apexwh/ui/tests/TestProductsFragment.java
+++ app/src/main/java/com/example/apexwh/ui/tests/TestProductsFragment.java
@@ -9,10 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import com.example.apexwh.HttpClient;
+import com.example.apexwh.HttpRequestJsonObjectInterface;
+import com.example.apexwh.JsonProcs;
 import com.example.apexwh.R;
 import com.example.apexwh.objects.DocumentLine;
 import com.example.apexwh.ui.adapters.DocumentLineAdapter;
 import com.example.apexwh.ui.products.ProductsFragment;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -36,6 +42,44 @@ public class TestProductsFragment extends ProductsFragment {
         setListUpdater(new ListUpdater() {
             @Override
             public void update(String name, String ref, ArrayList<DocumentLine> lines, ProgressBar progressBar, DocumentLineAdapter adapter) {
+
+                lines.clear();
+
+                HttpClient httpClient = new HttpClient(getContext());
+
+                httpClient.request_get("/hs/dta/obj?request=getLinesToTest&name=" + name + "&id=" + ref, new HttpRequestJsonObjectInterface() {
+
+                    @Override
+                    public void setProgressVisibility(int visibility) {
+
+                        progressBar.setVisibility(visibility);
+
+                    }
+
+                    @Override
+                    public void processResponse(JSONObject jsonObjectResponse) {
+
+                        JSONArray jsonArrayResponses = JsonProcs.getJsonArrayFromJsonObject(jsonObjectResponse, "responses");
+
+                        JSONObject jsonObjectItem = JsonProcs.getItemJSONArray(jsonArrayResponses, 0);
+
+                        JSONArray jsonArrayObjects = JsonProcs.getJsonArrayFromJsonObject(jsonObjectItem, "LinesToTest");
+
+                        for (int j = 0; j < jsonArrayObjects.length(); j++) {
+
+                            JSONObject objectItem = JsonProcs.getItemJSONArray(jsonArrayObjects, j);
+
+                            lines.add(DocumentLine.DocumentLineFromJson(objectItem));
+
+                        }
+
+                        adapter.notifyDataSetChanged();
+
+                    }
+
+                });
+
+
 
             }
         });

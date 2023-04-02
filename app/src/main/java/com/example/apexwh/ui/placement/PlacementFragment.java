4ba50729353d2a30f8945faec876ec71e99356bf
Mihail Ulyanenkov <uleman@mail.ru>
2023-04-02 15:16:31 +0300

    home place record scan cell

--- app/src/main/java/com/example/apexwh/ui/placement/PlacementFragment.java
+++ app/src/main/java/com/example/apexwh/ui/placement/PlacementFragment.java
@@ -2,19 +2,78 @@ package com.example.apexwh.ui.placement;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.example.apexwh.JsonProcs;
 import com.example.apexwh.R;
+import com.example.apexwh.RequestToServer;
 import com.example.apexwh.ui.ScanShtrihcodeFragment;
+import com.example.apexwh.ui.adapters.ScanCodeSetter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class PlacementFragment extends ScanShtrihcodeFragment {
 
 
     public PlacementFragment() {
-        super(R.layout.fragment_scan_shtrihcode);
+        super(R.layout.fragment_scan_cell_container);
+    }
+
+    private TextView tvCell, tvContainer;
+
+    private String cellRef, containerRef;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setOnCreateViewElements(new OnCreateViewElements() {
+            @Override
+            public void execute(View root) {
+
+                tvCell = root.findViewById(R.id.tvCell);
+                tvContainer = root.findViewById(R.id.tvContainer);
+
+            }
+        });
+
+        setScanCodeSetter(new ScanCodeSetter() {
+            @Override
+            public void setScanCode(String strCatName, int pos, int quantity) {
+
+                if (tvCell.getText().toString().isEmpty()){
+
+                    RequestToServer.executeRequestUW(getContext(), Request.Method.GET, "getErpSkladCells", "filter=" + strCatName, new JSONObject(), 1, response -> {
+
+                        JSONArray cells = JsonProcs.getJsonArrayFromJsonObject(response, "ErpSkladCells");
+
+                        if (cells.length() == 1){
+
+                            JSONObject cell = JsonProcs.getItemJSONArray(cells, 0);
+
+                            cellRef = JsonProcs.getStringFromJSON(cell, "ref");
+                            tvCell.setText(JsonProcs.getStringFromJSON(cell, "name"));
+
+                        }
+
+                    });
+
+                } else {
+
+
+                }
+
+
+
+            }
+        });
     }
 }

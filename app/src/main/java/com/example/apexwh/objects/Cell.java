4784ac26af4f15879921199971cb005dbefe8300
Mihail Ulyanenkov <uleman@mail.ru>
2023-07-08 07:59:12 +0300

    menu settings collect cells

--- app/src/main/java/com/example/apexwh/objects/Cell.java
+++ app/src/main/java/com/example/apexwh/objects/Cell.java
@@ -6,20 +6,43 @@ import org.json.JSONObject;
 
 public class Cell {
 
-    public String ref, name;
-    public Cell(String ref, String name) {
-
+    public String ref, name, line, section, rack, level, position;
+    public Boolean main;
+//    public Cell(String ref, String name) {
+//
+//        this.ref = ref;
+//        this.name = name;
+//        this.line = "";
+//        this.section = "";
+//        this.rack = "";
+//        this.level = "";
+//        this.position = "";
+//
+//    }
+//
+    public Cell(String ref, String name, String line, String section, String rack, String level, String position, Boolean main) {
         this.ref = ref;
         this.name = name;
-
+        this.line = line;
+        this.section = section;
+        this.rack = rack;
+        this.level = level;
+        this.position = position;
+        this.main = main;
     }
 
     public static Cell FromJson(JSONObject task_item) {
 
         String ref = JsonProcs.getStringFromJSON(task_item, "ref");
         String name = JsonProcs.getStringFromJSON(task_item, "name");
+        String line = JsonProcs.getStringFromJSON(task_item, "line");
+        String section = JsonProcs.getStringFromJSON(task_item, "section");
+        String rack = JsonProcs.getStringFromJSON(task_item, "rack");
+        String level = JsonProcs.getStringFromJSON(task_item, "level");
+        String position = JsonProcs.getStringFromJSON(task_item, "position");
+        Boolean main = JsonProcs.getBooleanFromJSON(task_item, "main");
 
-        return new Cell(ref, name);
+        return new Cell(ref, name, line, section, rack, level, position, main);
 
 
     }

edd2f6f5bb1256000914b16f42969d12cc3a97f9
mihail.u <mihail.u@ap-ex.ru>
2023-11-02 09:51:48 +0300

    Inventarization 2

---
+++ app/src/main/java/com/example/apexwh/objects/Inventarization.java
@@ -0,0 +1,55 @@
+package com.example.apexwh.objects;
+
+import com.example.apexwh.JsonProcs;
+
+import org.json.JSONObject;
+
+public class Inventarization {
+
+    public String ref, number, date, comment;
+    Cell cell;
+    Container container;
+    Product product;
+    Characteristic characteristic;
+
+
+    public int quantity;
+
+    public Inventarization(String ref, String number, String date, String comment, Cell cell, Container container, Product product, Characteristic characteristic, int quantity) {
+        this.ref = ref;
+        this.number = number;
+        this.date = date;
+        this.comment = comment;
+        this.cell = cell;
+        this.container = container;
+        this.product = product;
+        this.characteristic = characteristic;
+        this.quantity = quantity;
+    }
+
+    public static Inventarization FromJson(JSONObject task_item) {
+
+        String ref = JsonProcs.getStringFromJSON(task_item, "ref");
+        String number = JsonProcs.getStringFromJSON(task_item, "number");
+        String date = JsonProcs.getStringFromJSON(task_item, "date");
+        String comment = JsonProcs.getStringFromJSON(task_item, "comment");
+
+        Cell cell = Cell.FromJson(JsonProcs.getJsonObjectFromJsonObject(task_item, "cell"));
+
+        Container container = Container.FromJson(JsonProcs.getJsonObjectFromJsonObject(task_item, "container"));
+
+        Product product = Product.FromJson(JsonProcs.getJsonObjectFromJsonObject(task_item, "product"));
+        Characteristic characteristic = Characteristic.FromJson(JsonProcs.getJsonObjectFromJsonObject(task_item, "characteristic"));
+
+        int quantity = JsonProcs.getIntegerFromJSON(task_item, "quantity");
+
+        return new Inventarization(ref, number, date, comment, cell, container, product, characteristic, quantity);
+
+
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/example/apexwh/ui/Inventarizations/InventarizationsFragment.java
+++ app/src/main/java/com/example/apexwh/ui/Inventarizations/InventarizationsFragment.java
@@ -1,5 +1,8 @@
 package com.example.apexwh.ui.Inventarizations;
 
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -7,32 +10,125 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.android.volley.Request;
+import com.example.apexwh.JsonProcs;
 import com.example.apexwh.R;
+import com.example.apexwh.RequestToServer;
+import com.example.apexwh.objects.Inventarization;
+import com.example.apexwh.objects.Outcome;
+import com.example.apexwh.ui.adapters.DataAdapter;
+import com.example.apexwh.ui.adapters.ListFragment;
 
-public class InventarizationsFragment extends Fragment {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class InventarizationsFragment extends ListFragment<Inventarization> {
 
     private InventarizationsViewModel mViewModel;
 
-    public static InventarizationsFragment newInstance() {
-        return new InventarizationsFragment();
+    public InventarizationsFragment() {
+        super(R.layout.fragment_filter_list, R.layout.movers_service_list_item);
+
+        setListUpdater(new ListUpdater() {
+            @Override
+            public void update(ArrayList items, ProgressBar progressBar, DataAdapter adapter, String filter) {
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                int removed = items.size();
+
+                items.clear();
+
+                RequestToServer.executeRequestUW(getContext(), Request.Method.GET, "getErpSkladInventarizations",
+                        "filter=" + filter, new JSONObject(), 1,
+                        new RequestToServer.ResponseResultInterface() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                progressBar.setVisibility(View.GONE);
+
+                                JSONArray responseItems = JsonProcs.getJsonArrayFromJsonObject(response, "ErpSkladInventarizations");
+
+                                for (int j = 0; j < responseItems.length(); j++) {
+
+                                    JSONObject objectItem = JsonProcs.getItemJSONArray(responseItems, j);
+
+                                    items.add(Inventarization.FromJson(objectItem));
+
+                                }
+
+                                adapter.notifyItemRemoved(removed);
+
+
+                                adapter.notifyItemInserted(items.size());
+
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+
+
+            }
+        });
+
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inventarizations, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MenuHost menuHost = requireActivity();
+
+        menuHost.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                //menuInflater.inflate(R.menu.collect_list, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull android.view.MenuItem menuItem) {
+
+                boolean res = false;
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.miSettings:
+
+                        Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main).navigate(R.id.nav_settings);
+
+                        res = true;
+
+                        break;
+
+                    case R.id.miCollectByReceiver:
+
+                        Bundle bundle = new Bundle();
+                        bundle.putString("mode", "collect");
+                        Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main).navigate(R.id.nav_receiversListFragment, bundle);
+
+                        res = true;
+
+                        break;
+
+                };
+
+                return res;
+            }
+        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+
+
+
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(InventarizationsViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
 }

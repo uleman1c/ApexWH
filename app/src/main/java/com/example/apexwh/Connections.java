af1e6fcf3b5bf464b899fa81207390076d21a5ff
mihail.u <mihail.u@ap-ex.ru>
2024-03-12 15:27:55 +0300

    mainAddr

--- app/src/main/java/com/example/apexwh/Connections.java
+++ app/src/main/java/com/example/apexwh/Connections.java
@@ -5,15 +5,17 @@ import java.util.Map;
 
 public class Connections {
 
+    private static final String mainAddr = "http://duck.titan.loc/"; // https://ow.apx-service.ru/
+
     //public static String addr = "http://10.0.2.2/apex_tech_man";
-    public static String addr = "https://ow.apx-service.ru/tech_man";
+    public static String addr = mainAddr + "tech_man";
 
     public static String user= "exch";
 
     public static String password = "123456";
 
 
-    public static String fileAddr = "https://ow.apx-service.ru/apos/upload/uploadmp";
+    public static String fileAddr = mainAddr + "apos/upload/uploadmp";
 
     public static String fileAccessKey = "6a64edd9-56f8-11ed-8a49-bc305bf806da";
 
@@ -21,10 +23,10 @@ public class Connections {
 
 
 
-    public static String addrMob = "https://ow.apx-service.ru/tech_man/hs/mob/";
-    public static String addrDta = "https://ow.apx-service.ru/tech_man/hs/dta/obj/";
+    public static String addrMob = mainAddr + "tech_man/hs/mob/";
+    public static String addrDta = mainAddr + "tech_man/hs/dta/obj/";
     public static String addrDtaLoc = "http://192.168.182.238:8000/whdct";
-    public static String addrFiles = "https://ow.apx-service.ru/tech_man/hs/dta/files/";
+    public static String addrFiles = mainAddr + "tech_man/hs/dta/files/";
 
     public static Map<String, String> headers() {
 
